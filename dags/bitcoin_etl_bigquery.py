cd370634e20e71b407670921636505cbbe92dc99
Daiana de Paula Sales <daianapaulasales@gmail.com>
2025-09-24 22:50:02 -0300

    add concurrency

--- dags/bitcoin_etl_bigquery.py
+++ dags/bitcoin_etl_bigquery.py
@@ -106,6 +106,7 @@ def fetch_and_to_gbq():
     schedule="0 0 * * *",  # daily at 00:00 UTC
     start_date=pendulum.datetime(2025, 9, 17, tz="UTC"),
     catchup=True,
+    concurrency = 1,
     max_active_run = 1,
     owner_links={
         "Alex Lopes": "mailto:alexlopespereira@gmail.com",
